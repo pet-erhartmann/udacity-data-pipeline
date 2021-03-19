99d74b288d7641f78f73b54265d929bfcea020b4
Peter Hartmann <hartmann.p87@gmail.com>
2021-03-19 10:47:09 -0500

    add truncate option to dimension load

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -4,11 +4,14 @@ from airflow.utils.decorators import apply_defaults
 
 
 class LoadDimensionOperator(BaseOperator):
-
     ui_color = '#80BD9E'
     insert_sql = """
         INSERT INTO {table}
         {sql}
+    """
+
+    truncate_sql = """
+        TRUNCATE {table}
     """
 
     @apply_defaults
@@ -16,19 +19,26 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id="",
                  table="",
                  sql="",
+                 truncate_table=False,
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.sql = sql
+        self.truncate_table = truncate_table
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        if self.truncate_table:
+            self.log.info(f"Truncating dimension table: {self.table}")
+            redshift.run(LoadDimensionOperator.truncate_sql.format(self.table))
+
+        self.log.info(f"Loading dimension table {self.table}")
         formatted_sql = LoadDimensionOperator.insert_sql.format(
             table=self.table,
             sql=self.sql
         )
-        redshift.run(formatted_sql)
 
+        self.log.info(f"Executing Query: {formatted_sql}")
+        redshift.run(formatted_sql)
